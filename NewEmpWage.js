9f672d2b60e738483103b9e4ff4b9637cafdade7
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-12-29 20:40:37 +0530

    Store the daily wage along with Total Wage And save in Array

--- NewEmpWage.js
+++ NewEmpWage.js
@@ -15,15 +15,36 @@ function getWorkingHours(empCheck) {
         default:
             return 0;
     }
+} 
+
+function calDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
 }
 
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empdailyWageArr = new Array();
+
 while(totalWorkingDays <= MAX_HRS_IN_MONTH && 
-     totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays < NUM_OF_WORKING_DAYS){
         totalWorkingDays++;
-let empCheck = Math.floor(Math.random() * 10 ) % 3;
-totalEmpHrs += getWorkingHours(empCheck);
+        let empCheck = Math.floor(Math.random() * 10 ) % 3;
+        let empHrs = getWorkingHours(empCheck);
+        totalEmpHrs += empHrs
+        empdailyWageArr.push(calDailyWage(empHrs));
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+
+let empWage = calDailyWage(totalEmpHrs);
 console.log("Total Days: " + totalWorkingDays + " Total Hour: "+ totalEmpHrs + " Emp Wage:" + empWage);
+
+let totEmpWage = 0;
+function sum(dailyWage){
+    totEmpWage += dailyWage;
+}
+empdailyWageArr.forEach(sum);
+console.log("UC7 - Total days: " + totalWorkingDays + " total Hrs:" + totalEmpHrs + " Emp wage:" + totEmpWage);
+
+function totalWages(totalWage, dailyWage){
+    return totalWage + dailyWage;
+}
+console.log("UC7 - Emp Wage reduce:" + empdailyWageArr.reduce(totalWages, 0));
